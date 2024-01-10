31c4bc8ee9b5405aecc27e4c001692b206624ced
honeylalmj <mjhoneylal@gmail.com>
2024-01-10 13:07:32 +0100

    updated

--- view_data.py
+++ view_data.py
@@ -25,12 +25,9 @@ class DisplayPatientDataApp(MDApp):
     def __init__(self, patient_id, consultation_date, **kwargs):
         super().__init__(**kwargs)
         if getattr(sys, 'frozen', False):
-            # Running as a PyInstaller executable
-            base_path = sys._MEIPASS
+            base_path = os.path.dirname(sys.executable)
         else:
-            # Running as a script
-            base_path = os.path.abspath(".")
-        # script_dir = os.path.dirname(os.path.abspath(__file__))
+            base_path = os.path.dirname(__file__)
         self.patient_json_file_path = os.path.join(base_path,'patient_data.json')
         self.patient_id = patient_id
         self.consultation_date = consultation_date
